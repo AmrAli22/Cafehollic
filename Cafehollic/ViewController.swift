3ae2f9c72f04bbae90a0feffdde6427032833f36
Amr Ali <theamr.ali@gmail.com>
2018-07-21 09:57:35 -0700

    Initial Commit

---
+++ Cafehollic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cafehollic
+//
+//  Created by Sayed Abdo on 7/21/18.
+//  Copyright © 2018 TheAmrAli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
